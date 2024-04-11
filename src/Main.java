8cdf2b4486d3150c19452f795b2180333ac6d3f7
kryp100graf <kihaev1@gmail.com>
2024-04-11 15:28:41 +0400

    Переменные, задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         var text = "Привет, Мир!";
-
         System.out.println(text);
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println("\nЗадача 1:\n" + "\ndog = " + dog +
+                "\ncat = " + cat + "\npaper = " + paper);
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        System.out.println("\nЗадача 2:\n" + "\ndog = " + dog +
+                "\ncat = " + cat + "\npaper = " + paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+
+        System.out.println("\nЗадача 3:\n" + "\ndog = " + dog +
+                "\ncat = " + cat + "\npaper = " + paper);
     }
 }
