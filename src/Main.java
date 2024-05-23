ddf211c1b3ba3ed1b58c89bcdf6d0067a5c25235
Kryp100Graf <kihaev1@yandex.ru>
2024-05-23 15:17:21 +0400

    Задачи по циклам

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,44 @@ public class Main {
         System.out.println(text);
 
 //        Задачи по переменным
-        Variables.task1();
-        Variables.task4();
-        Variables.task5();
-        Variables.task6();
-        Variables.task7();
-        Variables.task8();
+//        Variables.task1();
+//        Variables.task4();
+//        Variables.task5();
+//        Variables.task6();
+//        Variables.task7();
+//        Variables.task8();
+//
+//        Conditions.task1();
+//        Conditions.task2();
+//        Conditions.task3();
+//        Conditions.task4();
+//        Conditions.task5();
+//        Conditions.task6();
+//        Conditions.task7();
+//        Conditions.task8();
+//        Conditions.task9();
+//        Conditions.task10();
+//        Conditions.task11();
+//        Conditions.task12();
 
-        Conditions.task1();
-        Conditions.task2();
-        Conditions.task3();
-        Conditions.task4();
-        Conditions.task5();
-        Conditions.task6();
-        Conditions.task7();
-        Conditions.task8();
-        Conditions.task9();
-        Conditions.task10();
-        Conditions.task11();
-        Conditions.task12();
-
+        Cycles.t1();
+        Cycles.t2();
+        Cycles.t3();
+        Cycles.t4();
+        Cycles.t5();
+        Cycles.t6();
+        Cycles.t7();
+        Cycles.t8();
+        Cycles.t9();
+        Cycles.t10();
+        Cycles.t11();
+        Cycles.t12();
+        Cycles.t13();
+        Cycles.t14();
+        Cycles.t15();
+        Cycles.t16();
+        Cycles.t17();
+        Cycles.t18();
 
     }
 
