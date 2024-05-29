1226a7e633ea41d6f257fabdb072378ae37f44d7
Kryp100Graf <kihaev1@yandex.ru>
2024-05-29 23:35:40 +0400

    поменял местами  строчки

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,24 @@ public class Main {
         var text = "Привет, Мир!";
         System.out.println(text);
 
-//        Задачи по переменным
-//        Variables.task1();
-//        Variables.task4();
-//        Variables.task5();
-//        Variables.task6();
-//        Variables.task7();
-//        Variables.task8();
+//        Cycles.t1();
+//        Cycles.t2();
+//        Cycles.t3();
+//        Cycles.t4();
+//        Cycles.t5();
+//        Cycles.t6();
+//        Cycles.t7();
+//        Cycles.t8();
+//        Cycles.t9();
+//        Cycles.t10();
+//        Cycles.t11();
+//        Cycles.t12();
+//        Cycles.t13();
+//        Cycles.t14();
+//        Cycles.t15();
+//        Cycles.t16();
+//        Cycles.t17();
+//        Cycles.t18();
 //
 //        Conditions.task1();
 //        Conditions.task2();
@@ -24,25 +35,14 @@ public class Main {
 //        Conditions.task10();
 //        Conditions.task11();
 //        Conditions.task12();
-
-        Cycles.t1();
-        Cycles.t2();
-        Cycles.t3();
-        Cycles.t4();
-        Cycles.t5();
-        Cycles.t6();
-        Cycles.t7();
-        Cycles.t8();
-        Cycles.t9();
-        Cycles.t10();
-        Cycles.t11();
-        Cycles.t12();
-        Cycles.t13();
-        Cycles.t14();
-        Cycles.t15();
-        Cycles.t16();
-        Cycles.t17();
-        Cycles.t18();
+//
+////        Задачи по переменным
+//        Variables.task1();
+//        Variables.task4();
+//        Variables.task5();
+//        Variables.task6();
+//        Variables.task7();
+//        Variables.task8();
 
     }
 
